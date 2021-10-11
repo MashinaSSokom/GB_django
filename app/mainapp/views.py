c9d559155a072871eb69ed5fe196fed82506175a
MashinaSSokom <skltan@mail.ru>
2021-10-11 22:23:12 +0800

    add dynamic links-menu in products

--- app/mainapp/views.py
+++ app/mainapp/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 
 def products(request):
-    context = {'title': 'Каталог'}
-
+    links_menu = [
+        {'href': 'products', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+    context = {'title': 'Каталог',
+               'links_menu': links_menu}
     return render(request, 'products.html', context=context)
