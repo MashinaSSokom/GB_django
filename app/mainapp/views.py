eb98a3dc9517beb07aa4c81f33b238bd8528b65c
MashinaSSokom <skltan@mail.ru>
2021-10-11 22:47:23 +0800

    add usage of file products.json to load realated-products in template

--- app/mainapp/views.py
+++ app/mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import json
 
 def products(request):
     links_menu = [
@@ -9,6 +9,14 @@ def products(request):
         {'href': 'products_modern', 'name': 'модерн'},
         {'href': 'products_classic', 'name': 'классика'},
     ]
+
+    related_products = []
+    with open(r'F:\projects\GB_django\app\products.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        for v in data.values():
+            related_products.append(v)
+
     context = {'title': 'Каталог',
-               'links_menu': links_menu}
+               'links_menu': links_menu,
+               'related_products': related_products}
     return render(request, 'products.html', context=context)
