853d0080c655148ec1ccd54088442d31e6640e82
MashinaSSokom <skltan@mail.ru>
2021-10-15 22:12:19 +0800

    add usage of models in templates

--- app/mainapp/views.py
+++ app/mainapp/views.py
@@ -1,20 +1,17 @@
 from django.shortcuts import render
 import json
 
-def products(request):
-    links_menu = [
-        {'href': 'products', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
+from . import models
 
-    related_products = []
-    with open(r'F:\projects\GB_django\app\products.json', 'r', encoding='utf-8') as f:
-        data = json.load(f)
-        for v in data.values():
-            related_products.append(v)
+
+def products(request):
+    links_menu = models.ProductCategory.objects.all()
+
+    related_products = models.Product.objects.all()[:4]
+    # with open(r'F:\projects\GB_django\app\products.json', 'r', encoding='utf-8') as f:
+    #     data = json.load(f)
+    #     for v in data.values():
+    #         related_products.append(v)
 
     context = {'title': 'Каталог',
                'links_menu': links_menu,
