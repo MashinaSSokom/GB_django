22cedc82655e5dc549412ed3b90a58883aa6d786
MashinaSSokom <skltan@mail.ru>
2021-10-24 20:09:42 +0800

    add dynamic products render and add detail product page

--- app/mainapp/urls.py
+++ app/mainapp/urls.py
@@ -1,12 +1,12 @@
 
 from django.urls import path
 
-from .views import products, product
+from .views import products_view, category_products_view, detail_product_view
 
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', products, name='products'),
-    path('<int:pk>', product, name='product'),
-
+    path('', products_view, name='products'),
+    path('category/<int:catid>', category_products_view, name='product'),
+    path('category/<int:catid>/detail/<int:productid>', detail_product_view, name='detail_product'),
 ]
