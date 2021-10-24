0cc04da843c836181acf5d6392c24728387d1c95
MashinaSSokom <skltan@mail.ru>
2021-10-24 23:24:51 +0800

    add basket model

--- app/basketapp/models.py
+++ app/basketapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from authapp.models import ShopUser
+from mainapp.models import Product
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(
+        to=ShopUser,
+        on_delete=models.CASCADE,
+        related_name='basket'
+    )
+    product = models.ForeignKey(
+        to=Product,
+        on_delete=models.CASCADE,
+        related_name='product'
+    )
+    quantity_in_basket = models.PositiveIntegerField(
+        default=0
+    )
+    created = models.DateField(
+        auto_now_add=True
+    )
+    updated = models.DateField(
+        auto_now=True
+    )
+
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзины'
+        ordering = ['id']
+
+    def __str__(self):
+        return f'{self.product} {self.quantity_in_basket} шт.'
+
