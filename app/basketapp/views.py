a873168b8183047151a3d9793809447040ebba20
MashinaSSokom <skltan@mail.ru>
2021-10-26 20:54:31 +0800

    add login required

--- app/basketapp/views.py
+++ app/basketapp/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 
 from mainapp.models import Product
 from . import models
 
 
+@login_required
 def basket_view(request):
     basket = models.Basket.objects.filter(user=request.user)
 
@@ -14,7 +16,11 @@ def basket_view(request):
     return render(request, 'basket.html', context)
 
 
+@login_required
 def basket_add_view(request, pk):
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('products:detail_product', args=[pk]))
+
     product = get_object_or_404(Product, pk=pk)
 
     basket = models.Basket.objects.filter(user=request.user, product=product).first()
@@ -28,6 +34,7 @@ def basket_add_view(request, pk):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_sub_view(request, pk):
     product = get_object_or_404(Product, pk=pk)
 
@@ -41,6 +48,7 @@ def basket_sub_view(request, pk):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove_view(request, pk):
     basket = get_object_or_404(models.Basket, pk=pk)
     basket.delete()
