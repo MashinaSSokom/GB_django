c91a1ed17aaf0c6525af3045d117ea28ba7bcf77
MashinaSSokom <skltan@mail.ru>
2021-10-29 23:18:24 +0800

    add user_passes_test and left navmenu for admin_staff

--- app/adminapp/views.py
+++ app/adminapp/views.py
@@ -1,8 +1,10 @@
 from authapp.models import ShopUser
 from django.shortcuts import get_object_or_404, render
 from mainapp.models import Product, ProductCategory
+from django.contrib.auth.decorators import user_passes_test
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def users(request):
     title = 'админка/пользователи'
 
