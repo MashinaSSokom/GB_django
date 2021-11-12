b79f50f729dbebf147267eeb1108cc8db8d83816
MashinaSSokom <skltan@mail.ru>
2021-11-12 18:43:39 +0800

    add userprofile model

--- app/authapp/models.py
+++ app/authapp/models.py
@@ -3,6 +3,8 @@ from datetime import timedelta
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.timezone import now
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class ShopUser(AbstractUser):
@@ -18,3 +20,36 @@ class ShopUser(AbstractUser):
 
     def is_activation_expired(self):
         return now() - self.date_joined <= timedelta(hours=48)
+
+
+class ShopUserProfile(models.Model):
+    MALE = "M"
+    FEMALE = "W"
+
+    GENDER_CHOICES = (
+        (MALE, 'М'),
+        (FEMALE, 'Ж')
+    )
+
+    user = models.OneToOneField(
+        ShopUser,
+        unique=True,
+        null=False,
+        db_index=True,
+        on_delete=models.CASCADE
+    )
+
+    tagline = models.CharField(verbose_name='тэги', max_length=128, blank=True)
+
+    about_me = models.TextField(verbose_name='о себе', max_length=512, blank=True)
+
+    gender = models.CharField(verbose_name='пол', max_length=1, choices=GENDER_CHOICES, blank=True)
+
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=ShopUser)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
