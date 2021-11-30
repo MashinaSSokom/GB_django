886d8f3b7a9755e7abb61334d889cec75cf76d0b
MashinaSSokom <skltan@mail.ru>
2021-11-30 19:55:15 +0800

    add DATABASE into env

--- app/project/settings.py
+++ app/project/settings.py
@@ -131,17 +131,22 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
-        'NAME': 'app',
-        'ENGINE': 'django.db.backends.postgresql',
-        'USER': 'postgres',
+DATABASE = env.str('DATABASE')
+if DATABASE == 'sqlite':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'NAME': 'app',
+            'ENGINE': 'django.db.backends.postgresql',
+            'USER': 'postgres',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
