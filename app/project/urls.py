32b9666149743cf5a15a8ff5329bab54508adb64
MashinaSSokom <skltan@mail.ru>
2021-12-03 18:30:38 +0700

    add mimtypes for ddt

--- app/project/urls.py
+++ app/project/urls.py
@@ -35,6 +35,10 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
+    import mimetypes
+
+    mimetypes.add_type("application/javascript", ".js", True)
+
 
     urlpatterns += [path('__debug__', include(debug_toolbar.urls))]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
